207237394e07f1b496c725a619fa91164a84bd80
Terekhov Ruslan <my.thaw@gmail.com>
2012-08-26 21:46:05 +0300

    User model with passwords, and user profile

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120826124701) do
+ActiveRecord::Schema.define(:version => 20120826180143) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
+    t.string   "salt"
+    t.string   "encrypted_password"
   end
 
 end
